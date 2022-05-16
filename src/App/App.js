ca4bcbd44c1236f288aed33ab54798658f066bf8
Vinny <vinnyshipley@gmail.com>
2022-05-16 16:15:14 -0700

    up to pulling API

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,43 @@
-import './App.css';
-import axios from'axios';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <h1>Test</h1>
-);
-}
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: ''
+    }
+  }
+
+  handleCitySubmit = async (e) => {
+    e.preventDefault();
+    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_API_KEY}&q=${this.state.city}&format=json`;
+    let cityInfo = await axios.get(url);
+    console.log(cityInfo[0]);
+  }
+
+  cityChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      city: e.target.value
+    });
+  }
+
+
+
+  render() {
+    return (
+      <>
+        <h1>City Explorer</h1>
+        <form onSubmit={this.handleCitySubmit}>
+          <label htmlFor="cityName">Pick a City</label>
+          <input type="text" id="cityName" onChange={this.cityChange}></input>
+          <button type="submit">Display City Data</button>
+        </form>
+      </>
+    );
+  };
+};
 
 export default App;
