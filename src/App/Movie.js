e3d598e067e31e82dead88f2d7d2012111d9db90
Vinny <vinnyshipley@gmail.com>
2022-05-28 18:32:44 -0700

    front passing movie info the front end but needs formatting into the carousel

--- src/App/Movie.js
+++ src/App/Movie.js
@@ -1,13 +1,28 @@
 import React from "react";
-// import Carousel from 'react-bootstrap/Carousel';
+import Carousel from 'react-bootstrap/Carousel';
 
 class Movie extends React.Component {
   render() {
     let passedMovieData = this.props.hasMovieData;
-    console.log(this.props.hasMovieData)
+    console.log(this.props.movieData)
     if (passedMovieData === true) {
     return (
-      <p>{console.log(this.props.hasMovieData)}</p>
+      <Carousel>
+        {this.props.movieData.map((i) => {
+
+          <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=First slide&bg=373940"
+      alt="First slide"
+      />
+    <Carousel.Caption>
+      <h3>`{this.props.movieData[i.title]}`</h3>
+      <p></p>
+    </Carousel.Caption>
+  </Carousel.Item>
+    })}
+  </Carousel>
     )
   }
     else {
